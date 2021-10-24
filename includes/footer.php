a60a0cb858486d21c404d2c587b12543b39daeea
Tushar Bindal <tusharbindal297@gmail.com>
2021-10-24 12:37:35 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2021 Online Library Management System |<a href="https://google.com/" target="_blank"  > Designed by : TUSHAR BINDAL, FARUKH</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
